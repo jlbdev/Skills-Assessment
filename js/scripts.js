4e150c22833f1be21d2b50e34f3fea8091bb7ff8
Ben Atkins <benjatkins13@gmail.com>
2018-07-25 14:38:21 -0500

    formatted pics and text below header, added fadein animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,104 @@
+
+
 $(document).ready(function() {
   let randNum = Math.floor(Math.random() * (3)+1)
 console.log("randNum",randNum)
 $("#hero-img").attr("src",`assets/HERO ${randNum}.jpg`)
-});
+console.log('here')
+$('#hero-area').fadeIn('slow')
+
+
+
+})
+
+var img1pos = $('#img2').offset().top;
+console.log("img1pos",img1pos)
+
+
+$(document).scroll(function() {
+
+  var activation_offset = 1
+var screen_height = $(window).height();
+
+// var max_scroll_height = $('body').height() - screen_height - 5
+var img1pos = $('#img1').offset().top;
+
+var img1act = img1pos - (screen_height * activation_offset)
+var img2pos = $('#img2').offset().top;
+console.log("img1pos",img2pos)
+var img2act = img2pos - (screen_height * activation_offset)
+var img3pos = $('#img3').offset().top;
+var img3act = img3pos - (screen_height * activation_offset)
+var img4pos = $('#img4').offset().top;
+var img4act = img4pos - (screen_height * activation_offset)
+var img5pos = $('#img5').offset().top;
+var img5act = img5pos - (screen_height * activation_offset)
+var iframepos = $('#iframe').offset().top;
+var iframeact = iframepos - (screen_height * activation_offset)
+var textPos = $('#text-right').offset().top;
+var textAct = textPos - (screen_height * activation_offset)
+var y_scroll_pos = window.pageYOffset
+var img1_in_view = y_scroll_pos > img1act;
+var img2_in_view = y_scroll_pos > img2act;
+var img3_in_view = y_scroll_pos > img3act; 
+var img4_in_view = y_scroll_pos > img4act;
+var img5_in_view = y_scroll_pos > img5act;
+var iframe_in_view = y_scroll_pos > iframeact;
+var text_in_view = y_scroll_pos > textAct;
+
+
+console.log(img1act,img2act,img3act,img4act,img5act)
+  console.log(img1_in_view,img2_in_view,img3_in_view,img4_in_view,img5_in_view)
+
+
+
+  if(img1_in_view){
+    $("#img1").fadeIn('slow');
+  }else{
+    $("#img1").fadeOut('fast');
+  }
+
+
+  if(img2_in_view){
+    console.log(img2_in_view,"img2")
+    $("#img2").fadeIn('slow');
+  }else{
+    $("#img2").fadeOut('fast');
+  }
+
+
+  if(img3_in_view){
+    $("#img3").fadeIn('slow');
+  }else{
+    $("#img3").fadeOut('fast');
+  }
+
+
+  if(img4_in_view){
+    $("#img4").fadeIn('slow');
+  }else{
+    $("#img4").fadeOut('fast');
+  }
+
+
+  if(img5_in_view){
+    $("#img5").fadeIn('slow');
+  }
+  else{
+    $("#img5").fadeOut('fast');
+  }
+
+  if(iframe_in_view){
+    $("#iframe").fadeIn('slow');
+  }else{
+    $("#iframe").fadeOut('fast');
+  }
+
+  if(text_in_view){
+    $("#text-right").fadeIn('slow');
+  }else{
+    $("#text-right").fadeOut('fast');
+  }
+}
+
+);
